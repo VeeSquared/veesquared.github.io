a61b6c1076a9e9710e6ceb9c97d3fae6ee1f506f
Vee <vval@users.noreply.github.com>
2020-09-01 23:17:02 -0700

    Added Slogan Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BannerComponent } from './@theme/components/banner/banner.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { SloganComponent } from './slogan/slogan.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    SloganComponent
   ],
   imports: [
     BrowserModule,
